1435d2ec58a0a6f15e0a6943e84096fb3a461af0
restrictor911 <ciulen666@gmail.com>
2016-10-04 15:54:35 +0300

    lesson_00_matrix_multiplication

--- src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
+++ src/main/java/ru/javaops/masterjava/matrix/MainMatrix.java
@@ -1,5 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
+
 /**
  * gkislin
  * 03.07.2016
@@ -13,11 +17,77 @@ public class MainMatrix {
         final int[][] matrixA = new int[MATRIX_SIZE][MATRIX_SIZE];
         final int[][] matrixB = new int[MATRIX_SIZE][MATRIX_SIZE];
 
+        MatrixUtil.fillMatrix(matrixA);
+        MatrixUtil.fillMatrix(matrixB);
+
         long start = System.currentTimeMillis();
         final int[][] matrixC =  MatrixUtil.singleThreadMultiply(matrixA, matrixB);
         System.out.println("Single thread multiplication time, sec: " + (System.currentTimeMillis() - start)/1000.);
 
-        // TODO implement parallel multiplication matrixA*matrixB
-        // TODO compare wih matrixC;
+        start = System.currentTimeMillis();
+
+        final int[][] matrixD = new int[MATRIX_SIZE][MATRIX_SIZE];
+
+        ExecutorService matrixExecutor = Executors.newFixedThreadPool(THREAD_NUMBER);
+        List<Future<RowResult>> futures = new LinkedList<>();
+        final CompletionService<RowResult> completionService = new ExecutorCompletionService<>(matrixExecutor);
+        for (int i = 0; i < MATRIX_SIZE; i++) {
+            final int row = i;
+            futures.add(completionService.submit(new Callable<RowResult>() {
+                private final RowResult rowResult = new RowResult(row, MATRIX_SIZE);
+
+                @Override
+                public RowResult call() throws Exception {
+                    for (int j = 0; j < MATRIX_SIZE; j++) {
+                        int sum = 0;
+                        for (int k = 0; k < MATRIX_SIZE; k++) {
+                            sum += matrixA[row][k] * matrixB[k][j];
+                        }
+                        rowResult.getRow()[j] = sum;
+                    }
+                    return rowResult;
+                }
+            }));
+        }
+        while (!futures.isEmpty()) {
+            try {
+                Future<RowResult> future = completionService.poll(5, TimeUnit.SECONDS);
+                if (future == null) {
+                    System.out.println("Error!!!");
+                    throw new InterruptedException();
+                }
+                futures.remove(future);
+                RowResult rowResult = future.get();
+                matrixD[rowResult.getRowNumber()] = rowResult.getRow();
+            } catch (ExecutionException ee) {
+                System.out.println("Execution exception!!!");
+            } catch (InterruptedException ie) {
+                System.out.println("Interrupted exception!!!");
+            }
+        }
+        matrixExecutor.shutdown();
+
+        System.out.println("Multiple threads multiplication time, sec: " + (System.currentTimeMillis() - start)/1000.);
+
+        boolean multResult = MatrixUtil.compareMatrices(matrixC, matrixD);
+        System.out.println(multResult ? "Matrices are equal." : "Matrices are not equal.");
+    }
+
+    private static class RowResult {
+        private final int rowNumber;
+        private final int[] row;
+
+        RowResult(int rowNumber, int matrixSize) {
+            this.rowNumber = rowNumber;
+            this.row = new int[matrixSize];
+        }
+
+        int getRowNumber() {
+            return rowNumber;
+        }
+
+        int[] getRow() {
+            return row;
+        }
     }
 }
--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -21,4 +21,22 @@ public class MatrixUtil {
         }
         return matrixC;
     }
+
+    public static void fillMatrix(int[][] matrix) {
+        int size = matrix.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = (int)(10 * Math.random() + 1);
+            }
+        }
+    }
+
+    public static boolean compareMatrices(int[][] matrix1, int[][] matrix2) {
+        for (int i = 0; i < matrix1.length; i++) {
+            for (int j = 0; j < matrix1.length; j++) {
+                if (matrix1[i][j] != matrix2[i][j]) return false;
+            }
+        }
+        return true;
+    }
 }
