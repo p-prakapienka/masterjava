71353329225d5fe35f3460387e077469a0e7b45f
p-prakapienka <prakapienka.work@gmail.com>
2017-05-26 18:05:15 +0300

    8_2_HW7_app_conf

--- common/src/main/java/ru/javaops/masterjava/config/Configs.java
+++ common/src/main/java/ru/javaops/masterjava/config/Configs.java
@@ -3,6 +3,8 @@ package ru.javaops.masterjava.config;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
+import java.io.File;
+
 /**
  * gkislin
  * 01.11.2016
@@ -15,5 +17,13 @@ public class Configs {
 
     public static Config getConfig(String resource, String domain) {
         return getConfig(resource).getConfig(domain);
+    }
+
+    public static File getConfigFile(String path) {
+        return new File(AppConfig.appConfig.getString("configDir"), path);
+    }
+
+    private static class AppConfig {
+        private static final Config appConfig = getConfig("app.conf", "app");
     }
 }
